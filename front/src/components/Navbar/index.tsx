11a304e7c09455105bcf6c191b651b34e935851c
Eilin Seth <aksun989@gmail.com>
2025-02-13 15:51:55 +0700

    Styling lagi pada sidebar

--- front/src/components/Navbar/index.tsx
+++ front/src/components/Navbar/index.tsx
@@ -1,3 +1,7 @@
+import About from "../../assets/svg/about.svg"
+import Community from "../../assets/svg/community.svg"
+import Login from "../../assets/svg/login.svg"
+import Sign from "../../assets/svg/signature.svg"
 import classNames from "classnames";
 import { useState } from "react";
 
@@ -33,34 +37,40 @@ export default function Navbar (){
         </button>
 
         <div
-          className={`z-10 fixed top-0 right-0 w-[40%] h-full border-l-2 border-sky-300 md:border-none  bg-slate-300 flex flex-col gap-6 md:bg-transparent text-black text-sm p-4 transform transition-transform md:static md:flex-row md:flex md:translate-0 md:items-center md:justify-end md:gap-8 md:mx-[5%] ${
+          className={`z-10 fixed top-0 right-0 w-[40%] h-full border-l-2 md:border-none bg-darkSideBar flex flex-col gap-6 md:bg-transparent text-black text-sm p-4 transform transition-transform md:static md:flex-row md:flex md:translate-0 md:items-center md:justify-end md:gap-8 md:mx-[5%] ${
             isOpen ? 'translate-0' : 'translate-x-full'
           }`}
         >
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="fixed p-1 w-[20%] right-6 top-[9px] font-semibold rounded-lg mb-10 text-sm bg-red-500 text-white md:hidden font-[Poppins] "
+            className="fixed p-1 w-[20%] right-6 top-[2.8%] font-semibold rounded-lg mb-10 text-sm bg-slate-500 text-white md:hidden font-[Poppins] "
           >
             X
           </button>
 
-          <a href="#" className="md:w-fit mt-15 md:mt-0 bg-slate-500  text-white font-semibold py-1 px-2 rounded-md">
-            About
+          <a href="#" className="flex gap-3 items-center  md:w-fit md:pr-3 mt-15 md:mt-0 bg-slate-500  text-white font-semibold py-1 px-2 rounded-md">
+            <img src={About} className="w-5 h-5 invert" />
+            <p>About</p>
           </a>
-          <a href="#" className="bg-red-700  md:w-fit text-white font-semibold py-1 px-2 rounded-md">
-            Community
-          </a>
-          <a
-            href="#"
-            className="bg-green-700  md:w-fit text-white font-semibold py-1 px-2 rounded-md"
-          >
-            Login
+          <a href="#" className="bg-red-700 flex gap-3 items-center md:w-fit text-white font-semibold py-1 px-2 rounded-md">
+            <img src={Community} className="w-5 h-5 invert" />
+            <p>Community</p>
           </a>
           <a
             href="#"
-            className="bg-gray-700  md:w-fit text-white font-semibold py-1 px-2 rounded-md"
+            className="bg-green-700 flex gap-3 items-center md:w-fit text-white font-semibold py-1 px-2 rounded-md"
           >
-            Register
+            <img src={Login} className="w-5 h-5 invert" />
+
+            <p>Login</p>
+          </a>
+          <a
+            href="#"
+            className="bg-gray-700 flex gap-3 items-center md:w-fit text-white font-semibold py-1 px-2 rounded-md"
+          >
+            <img src={Sign} className="w-5 h-5 invert" />
+
+            <p>Register</p>
           </a>
         </div>
       </nav>
