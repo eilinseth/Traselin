2f6a0139019d141f68858aacb05b4186eafc4530
Eilin Seth <aksun989@gmail.com>
2025-02-17 17:27:17 +0700

    Membuat page dummy untuk Search

--- front/src/pages/Search/index.tsx
+++ front/src/pages/Search/index.tsx
@@ -1,8 +1,23 @@
+import SearchIcon from "../../assets/svg/search.svg"
 
 export default function Search(){
     return(
-        <div className="flex items-center justify-center min-h-screen w-full mx-auto p-2 ">
-            <p className="text-4xl">WAKEOAWKE</p>
+        <div className="pb-20 mx-auto px-[4%] bg-[url('background/background.jpg')] bg-cover bg-center min-h-screen flex items-center justify-center flex-wrap">
+            <div className=" w-[70%] h-[10%] mt-20 mx-auto p-2 ">
+                <div className="flex items-center p-2 bg-gray-100 rounded-xl gap-2">
+                    <img src={SearchIcon} className="h-4 w-4 " />
+                   <input type="search" className="text-slate-900  w-[30rem] p-1 pl-2" placeholder="Search here ..." />
+                </div>
+                
+            </div>
+            <div className=" grid md:grid-cols-3 grid-cols-2 items-center gap-4 w-full mt-10">
+                <div className="bg-white h-80">awd</div>
+                <div className="bg-white h-80">awd</div>
+                <div className="bg-white h-80">awd</div>
+            </div>
+            
+
+
         </div>
     )
 }
